60642ec2e4536b2c3fef3b49cf86e7b8bcb40349
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-02-22 17:39:25 +0100

    connect controller to repositories

--- src/main/java/com/plapp/greenhouseservice/repositories/PlantRepository.java
+++ src/main/java/com/plapp/greenhouseservice/repositories/PlantRepository.java
@@ -3,6 +3,12 @@ package com.plapp.greenhouseservice.repositories;
 import com.plapp.entities.greenhouse.Plant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PlantRepository extends JpaRepository<Plant, Integer> {
+import java.util.List;
+import java.util.Optional;
 
+public interface PlantRepository extends JpaRepository<Plant, Integer> {
+    List<Plant> findByOwner(long owner);
+
+    boolean existsById(long id);
+    Optional<Plant> findById(long id);
 }
--- src/main/java/com/plapp/greenhouseservice/repositories/StoryboardRepository.java
+++ src/main/java/com/plapp/greenhouseservice/repositories/StoryboardRepository.java
@@ -1,7 +1,12 @@
 package com.plapp.greenhouseservice.repositories;
 
+import com.plapp.entities.greenhouse.Plant;
 import com.plapp.entities.greenhouse.Storyboard;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StoryboardRepository extends JpaRepository<Storyboard, Integer> {
+    Storyboard findByPlant(Plant plant);
+
+    boolean existsById(Long id);
+    void deleteById(Long id);
 }

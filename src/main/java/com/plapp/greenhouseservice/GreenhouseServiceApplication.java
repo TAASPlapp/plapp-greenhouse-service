2bb65b41d2ff42f454488b1211b090c97bc60be4
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-02-22 14:05:18 +0100

    first commit

---
+++ src/main/java/com/plapp/greenhouseservice/GreenhouseServiceApplication.java
@@ -0,0 +1,13 @@
+package com.plapp.greenhouseservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenhouseServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenhouseServiceApplication.class, args);
+    }
+
+}

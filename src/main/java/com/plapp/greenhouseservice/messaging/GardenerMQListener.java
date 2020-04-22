4341aa9c19f23f10ea12ec1cc14236d23d753026
Carlo Alberto Barbano <carlo.alberto.barbano@outlook.com>
2020-04-22 19:34:46 +0200

    update plant status based on diagnosis

--- src/main/java/com/plapp/greenhouseservice/messaging/GardenerMQListener.java
+++ src/main/java/com/plapp/greenhouseservice/messaging/GardenerMQListener.java
@@ -4,15 +4,23 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.plapp.entities.exceptions.ActorNotFoundException;
 import com.plapp.entities.greenhouse.Plant;
+import com.plapp.entities.greenhouse.Storyboard;
+import com.plapp.entities.greenhouse.StoryboardItem;
 import com.plapp.entities.messaging.DiagnosisMQDTO;
 import com.plapp.entities.messaging.ScheduleActionMQDTO;
 import com.plapp.entities.schedules.Diagnosis;
 import com.plapp.entities.schedules.ScheduleAction;
 import com.plapp.greenhouseservice.config.RabbitMQConfig;
+import com.plapp.greenhouseservice.entities.StoryboardDPO;
+import com.plapp.greenhouseservice.entities.StoryboardItemDPO;
 import com.plapp.greenhouseservice.mappers.DiagnosisMapper;
 import com.plapp.greenhouseservice.mappers.ScheduleActionMapper;
+import com.plapp.greenhouseservice.repositories.StoryboardItemRepository;
+import com.plapp.greenhouseservice.repositories.StoryboardRepository;
 import com.plapp.greenhouseservice.services.NotificationService;
 import com.plapp.greenhouseservice.services.PlantService;
+import com.plapp.greenhouseservice.services.StoryboardItemService;
+import com.plapp.greenhouseservice.services.StoryboardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.AmqpRejectAndDontRequeueException;
 import org.springframework.amqp.core.*;
@@ -23,6 +31,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class GardenerMQListener {
@@ -32,6 +42,10 @@ public class GardenerMQListener {
 
     private final ScheduleActionMapper scheduleActionMapper;
     private final DiagnosisMapper diagnosisMapper;
+
+    private final PlantService plantService;
+    private final StoryboardRepository storyboardRepository;
+    private final StoryboardItemRepository storyboardItemRepository;
 
     @Bean
     public Queue gardenerQueue() {
@@ -67,8 +81,29 @@ public class GardenerMQListener {
 
         } else if (message.getMessageProperties().getHeaders().containsValue("com.plapp.entities.schedules.Diagnosis")) {
             Diagnosis diagnosis = new ObjectMapper().readValue(body, Diagnosis.class);
-
             System.out.println(String.format("Received diagnosis from %s: %s", rabbitMQConfig.getGardenerQueueName(), diagnosis));
+
+            System.out.println("Updating plant status");
+            diagnosis.isIll();
+            Plant plant = plantService.getPlant(Integer.parseInt(diagnosis.getPlantId()));
+            StoryboardDPO storyboard = storyboardRepository.findByPlant(plant);
+            List<StoryboardItemDPO> storyboardItemDPOList = storyboard.getStoryboardItems();
+            StoryboardItemDPO storyboardItemDPO = storyboardItemDPOList.get(storyboardItemDPOList.size() - 1);
+
+            if (diagnosis.isIll()) {
+                System.out.println("Plant is sick");
+                plant.setStatus(Plant.PlantHealthStatus.SICK);
+                storyboardItemDPO.setStatus(Plant.PlantHealthStatus.SICK);
+
+            } else {
+                System.out.println("Plant is healthy");
+                plant.setStatus(Plant.PlantHealthStatus.HEALTHY);
+                storyboardItemDPO.setStatus(Plant.PlantHealthStatus.HEALTHY);
+            }
+
+            plantService.addPlant(plant);
+            storyboardItemRepository.save(storyboardItemDPO);
+
 
             try {
                 DiagnosisMQDTO diagnosisMQDTO = diagnosisMapper.diagnosisToDiagnosisMQDTO(diagnosis);
